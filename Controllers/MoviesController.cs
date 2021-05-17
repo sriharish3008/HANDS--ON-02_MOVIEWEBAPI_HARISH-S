9cac136503324eac6a5d248f7a25381c286664cf
sriharish3008 <sriharish3008@gmail.com>
2021-05-17 17:13:23 +0530

    first commit

---
+++ Controllers/MoviesController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace MovieWebAPI.Controllers
+{
+    public class MoviesController : ApiController
+    {
+        public string Get()
+        {
+            return "Hello from Web API";
+        }
+        // GET: Movies
+        //public List<string> GetMovies()
+        //{
+        //    return new List<string> { "Titanic", "Mission Impossible", "Matrix" };
+        //}
+    }
+}
